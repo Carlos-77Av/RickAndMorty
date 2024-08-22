dac83c211ef20ea0510448cac730789c5393d8d3
Carlos-77Av <carlos.av.bit@gmail.com>
2024-08-22 01:44:27 -0600

    ♻️ Refactor ContentView to CharacterListView

--- RickAndMorty/RickAndMorty/Presentation/CharacterList/CharacterListView.swift
+++ RickAndMorty/RickAndMorty/Presentation/CharacterList/CharacterListView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  CharacterListView.swift
 //  RickAndMorty
 //
 //  Created by Carlos Alvarez on 21/8/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CharacterListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    CharacterListView()
 }
