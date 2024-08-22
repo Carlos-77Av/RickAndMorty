841be44c6cac3bec0f0d1e0da85e728c579c46df
Carlos-77Av <carlos.av.bit@gmail.com>
2024-08-22 00:23:14 -0600

    ➕ Add Apollo dependency using SPM

---
+++ RickAndMorty/rickandmortyapi/Sources/Operations/Queries/CharacterListQuery.graphql.swift
@@ -0,0 +1,101 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+@_exported import ApolloAPI
+
+public class CharacterListQuery: GraphQLQuery {
+  public static let operationName: String = "CharacterListQuery"
+  public static let operationDocument: ApolloAPI.OperationDocument = .init(
+    definition: .init(
+      #"query CharacterListQuery($page: Int) { characters(page: $page) { __typename info { __typename count pages next } results { __typename ...CharacterInfo } } }"#,
+      fragments: [CharacterInfo.self]
+    ))
+
+  public var page: GraphQLNullable<Int>
+
+  public init(page: GraphQLNullable<Int>) {
+    self.page = page
+  }
+
+  public var __variables: Variables? { ["page": page] }
+
+  public struct Data: Rickandmortyapi.SelectionSet {
+    public let __data: DataDict
+    public init(_dataDict: DataDict) { __data = _dataDict }
+
+    public static var __parentType: any ApolloAPI.ParentType { Rickandmortyapi.Objects.Query }
+    public static var __selections: [ApolloAPI.Selection] { [
+      .field("characters", Characters?.self, arguments: ["page": .variable("page")]),
+    ] }
+
+    /// Get the list of all characters
+    public var characters: Characters? { __data["characters"] }
+
+    /// Characters
+    ///
+    /// Parent Type: `Characters`
+    public struct Characters: Rickandmortyapi.SelectionSet {
+      public let __data: DataDict
+      public init(_dataDict: DataDict) { __data = _dataDict }
+
+      public static var __parentType: any ApolloAPI.ParentType { Rickandmortyapi.Objects.Characters }
+      public static var __selections: [ApolloAPI.Selection] { [
+        .field("__typename", String.self),
+        .field("info", Info?.self),
+        .field("results", [Result?]?.self),
+      ] }
+
+      public var info: Info? { __data["info"] }
+      public var results: [Result?]? { __data["results"] }
+
+      /// Characters.Info
+      ///
+      /// Parent Type: `Info`
+      public struct Info: Rickandmortyapi.SelectionSet {
+        public let __data: DataDict
+        public init(_dataDict: DataDict) { __data = _dataDict }
+
+        public static var __parentType: any ApolloAPI.ParentType { Rickandmortyapi.Objects.Info }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("__typename", String.self),
+          .field("count", Int?.self),
+          .field("pages", Int?.self),
+          .field("next", Int?.self),
+        ] }
+
+        /// The length of the response.
+        public var count: Int? { __data["count"] }
+        /// The amount of pages.
+        public var pages: Int? { __data["pages"] }
+        /// Number of the next page (if it exists)
+        public var next: Int? { __data["next"] }
+      }
+
+      /// Characters.Result
+      ///
+      /// Parent Type: `Character`
+      public struct Result: Rickandmortyapi.SelectionSet {
+        public let __data: DataDict
+        public init(_dataDict: DataDict) { __data = _dataDict }
+
+        public static var __parentType: any ApolloAPI.ParentType { Rickandmortyapi.Objects.Character }
+        public static var __selections: [ApolloAPI.Selection] { [
+          .field("__typename", String.self),
+          .fragment(CharacterInfo.self),
+        ] }
+
+        /// The id of the character.
+        public var id: Rickandmortyapi.ID? { __data["id"] }
+        /// The name of the character.
+        public var name: String? { __data["name"] }
+
+        public struct Fragments: FragmentContainer {
+          public let __data: DataDict
+          public init(_dataDict: DataDict) { __data = _dataDict }
+
+          public var characterInfo: CharacterInfo { _toFragment() }
+        }
+      }
+    }
+  }
+}
