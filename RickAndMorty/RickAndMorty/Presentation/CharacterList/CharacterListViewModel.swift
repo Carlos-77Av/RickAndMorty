0d82bca04fce67925f503d8fdac0ee81d5f81296
Carlos-77Av <carlos.av.bit@gmail.com>
2024-08-22 01:58:18 -0600

    🚧 Add Character List ViewModel

---
+++ RickAndMorty/RickAndMorty/Presentation/CharacterList/CharacterListViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  CharacterListViewModel.swift
+//  RickAndMorty
+//
+//  Created by Carlos Alvarez on 22/8/24.
+//
+
+import Foundation
+import Combine
+
+struct CharacterListViewModelDependencies: CharacterListViewModel.Dependencies {
+    var characterUseCase: GetCharacterListUseCaseType = GetCharacterListUseCase(
+        repository: CharacterRepository(dependencies: RepositoryDependencies()))
+}
+
+class CharacterListViewModel: ObservableObject {
+    typealias Dependencies = HasGetCharacterListUseCase
+    
+    @Published private(set) var characterList = [Character]()
+    
+    private(set) var dependencies: Dependencies
+    private var cancellables = Set<AnyCancellable>()
+    private var currentPage: Int = 1
+    
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
+    
+    func fetchCharacters() {
+        dependencies
+            .characterUseCase
+            .execute(page: currentPage)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    print(error.localizedDescription)
+                }
+            }, receiveValue: { [weak self] characters in
+                guard let self = self, !characters.isEmpty else { return }
+                
+                self.characterList.append(contentsOf: characters)
+                
+                print(characters)
+            })
+            .store(in: &cancellables)
+    }
+}
